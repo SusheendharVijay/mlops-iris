b6bc4dcf7476e7a9e39497fa9c67887d86575d0b
Sushi <susheendharvj@gmail.com>
2021-07-10 15:26:12 +0530

    best clf working

--- ml_utils.py
+++ ml_utils.py
@@ -4,8 +4,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 # define a Gaussain NB classifier
-clf = None
-
+clf = GaussianNB()
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
 r_classes = {y: x for x, y in classes.items()}
@@ -30,7 +29,7 @@ def load_model():
 
     clf = rf if acc_rf > acc_gnb else gnb
     best_acc = acc_rf if acc_rf > acc_gnb else acc_gnb
-    print(f"{clf.__name__} trained with best accuracy: {round(best_acc, 3)}")
+    print(f"{clf} trained with best accuracy: {round(best_acc, 3)}")
 
 
 # function to predict the flower using the model
@@ -50,6 +49,3 @@ def retrain(data):
 
     # fit the classifier again based on the new data obtained
     clf.fit(X, y)
-
-
-load_model()
