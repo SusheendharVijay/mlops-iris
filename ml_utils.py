cf1e0a5a3cec72013a83c630b2f67d3dbcd4282e
Sushi <susheendharvj@gmail.com>
2021-07-10 15:14:55 +0530

    best clf code finished, with errors

--- ml_utils.py
+++ ml_utils.py
@@ -2,26 +2,35 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-
+from sklearn.ensemble import RandomForestClassifier
 # define a Gaussain NB classifier
-clf = GaussianNB()
+clf = None
 
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
 r_classes = {y: x for x, y in classes.items()}
 
 # function to train and load the model during startup
+
+
 def load_model():
     # load the dataset from the official sklearn datasets
     X, y = datasets.load_iris(return_X_y=True)
 
     # do the test-train split and train the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf.fit(X_train, y_train)
+    rf = RandomForestClassifier()
+    gnb = GaussianNB()
+    rf.fit(X_train, y_train)
+    gnb.fit(X_train, y_train)
 
     # calculate the print the accuracy score
-    acc = accuracy_score(y_test, clf.predict(X_test))
-    print(f"Model trained with accuracy: {round(acc, 3)}")
+    acc_rf = accuracy_score(y_test, rf.predict(X_test))
+    acc_gnb = accuracy_score(y_test, gnb.predict(X_test))
+
+    clf = rf if acc_rf > acc_gnb else gnb
+    best_acc = acc_rf if acc_rf > acc_gnb else acc_gnb
+    print(f"{clf.__name__} trained with best accuracy: {round(best_acc, 3)}")
 
 
 # function to predict the flower using the model
@@ -32,6 +41,8 @@ def predict(query_data):
     return classes[prediction]
 
 # function to retrain the model as part of the feedback loop
+
+
 def retrain(data):
     # pull out the relevant X and y from the FeedbackIn object
     X = [list(d.dict().values())[:-1] for d in data]
@@ -39,3 +50,6 @@ def retrain(data):
 
     # fit the classifier again based on the new data obtained
     clf.fit(X, y)
+
+
+load_model()
